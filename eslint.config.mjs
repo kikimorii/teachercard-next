878bbfaedd7340fbc8c10154a04198037ba31226
Oleg Fateev <fateev010405@gmail.com>
2025-12-04 04:06:26 +0300

    Add linters

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,22 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import prettier from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+	...nextVitals,
+	globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
+	{
+		extends: [prettier],
+		plugins: {
+			prettier: prettierPlugin,
+		},
+		rules: {
+			'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+			'prettier/prettier': 'error',
+		},
+	},
+	prettier,
 ]);
 
 export default eslintConfig;
