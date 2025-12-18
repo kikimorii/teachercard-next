0a2e1c4c0e5e2c200749aad709c8bb3a8761057d
Oleg <fateev010405@gmail.com>
2025-12-18 15:03:17 +0300

    Немного изменил стили для визуальной красоты на desktop

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import { ClientProvider } from '@/components/layout/ClienProvider';
 import { PersonInfo, PersonTitle } from '@/components';
+import styles from './page.module.scss';
 
 export default async function Home(props) {
 	const searchParams = await props.searchParams;
@@ -13,8 +14,10 @@ export default async function Home(props) {
 
 	return (
 		<ClientProvider data={data}>
-			<PersonTitle />
-			<PersonInfo />
+			<div className={styles.pageWrapper}>
+				<PersonTitle />
+				<PersonInfo />
+			</div>
 		</ClientProvider>
 	);
 }
--- components/PersonInfo/PersonInfo.jsx
+++ components/PersonInfo/PersonInfo.jsx
@@ -1,10 +1,14 @@
 'use client';
 import { PersonShare, PersonInfoList } from '@/components';
+const styles = {
+	backgroundColor: '#FFFFFF',
+	borderRadius: '20px',
+	padding: '24px',
+};
 
 export const PersonInfo = () => {
 	return (
-		<div>
-			<p>PersonInfo</p>
+		<div style={styles}>
 			<PersonShare />
 			<PersonInfoList />
 		</div>
