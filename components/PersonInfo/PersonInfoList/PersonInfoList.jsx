63caa02a554accb4b79edfc7c75615dfa3a786a2
Oleg Fateev <fateev010405@gmail.com>
2026-01-02 09:21:12 +0300

    hotfix: отображение MAX, должность с заглавной

--- components/PersonInfo/PersonInfoList/PersonInfoList.jsx
+++ components/PersonInfo/PersonInfoList/PersonInfoList.jsx
@@ -13,19 +13,26 @@ export const PersonInfoList = () => {
 			case 'OK':
 				return `https://ok.ru/${userID}`;
 			case 'MAX':
-				return `https://max.ru/id${userID}`;
+				return `https://max.ru/u/${userID}`;
 			default:
 				return '#';
 		}
 	};
 
+	const capitalizeFirstLetter = (str) => {
+		if (!str) return '';
+		return str.charAt(0).toUpperCase() + str.slice(1);
+	};
+
 	return (
 		<ul className={styles.personInfoList}>
-			<li className={styles.post}>{title}</li>
+			<li className={styles.post}>{capitalizeFirstLetter(title)}</li>
 			<li className={styles.location}>{location}</li>
 			{socials.map((element, index) => (
 				<li key={index} className={styles[element.type.toLowerCase()]}>
-					<a href={getHref(element)}>{element.userID}</a>
+					<a href={getHref(element)}>
+						{element.type === 'MAX' ? element.type : element.userID}
+					</a>
 				</li>
 			))}
 		</ul>
